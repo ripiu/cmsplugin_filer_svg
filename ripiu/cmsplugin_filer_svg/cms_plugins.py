32d8a6d7cb102e37034fd6fd24758a18e07682c9
geobaldi <geobaldi@gmail.com>
2017-11-26 18:20:30 +0100

    pep8

--- ripiu/cmsplugin_filer_svg/cms_plugins.py
+++ ripiu/cmsplugin_filer_svg/cms_plugins.py
@@ -1,7 +1,10 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
+
 from django.utils.translation import ugettext_lazy as _
+
 from .models import FilerSvgInlinePluginModel, FilerSvgImagePluginModel
+
 
 class FilerSvgInlinePlugin(CMSPluginBase):
     model = FilerSvgInlinePluginModel
@@ -10,7 +13,9 @@ class FilerSvgInlinePlugin(CMSPluginBase):
     render_template = 'ripiu/cmsplugin_filer_svg/inline.svg'
     allow_children = False
 
+
 plugin_pool.register_plugin(FilerSvgInlinePlugin)
+
 
 class FilerSvgImagePlugin(CMSPluginBase):
     model = FilerSvgImagePluginModel
@@ -18,5 +23,6 @@ class FilerSvgImagePlugin(CMSPluginBase):
     module = 'Filer'
     render_template = 'ripiu/cmsplugin_filer_svg/ref.html'
     allow_children = False
+
 
 plugin_pool.register_plugin(FilerSvgImagePlugin)
--- ripiu/cmsplugin_filer_svg/models.py
+++ ripiu/cmsplugin_filer_svg/models.py
@@ -1,54 +1,60 @@
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
 from cms.models import CMSPlugin
 from filer.fields.file import FilerFileField
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 class SVGPluginModel(CMSPlugin):
     """
     Base SVG plugin model
     """
-    
+
     file = FilerFileField(
-        blank = False, null = False, verbose_name = _('SVG file'),
-        help_text = _('Provide an SVG file')
+        blank=False, null=False, verbose_name=_('SVG file'),
+        help_text=_('Provide an SVG file')
     )
-    
+
     class Meta:
         abstract = True
+
 
 class FilerSvgInlinePluginModel(SVGPluginModel):
     """
     An inline SVG image
     """
-    
+
     def get_svg_code(self):
         try:
             return self.file.file.read()
         except IOError:
-            return ""
-    
+            return ''
+
     def __str__(self):
         return u'Inline SVG'
-    
+
     class Meta:
-        verbose_name = _("Inline SVG image")
-        verbose_name_plural = _("Inline SVG images")
+        verbose_name = _('Inline SVG image')
+        verbose_name_plural = _('Inline SVG images')
+
 
 class FilerSvgImagePluginModel(SVGPluginModel):
     """
     An SVG image
     """
-    
+
     # see cmsplugin_filer_image.models.FilerImage
-    alt_text = models.CharField(_("alt text"), null=False, blank=False, max_length=255)
-    
-    width = models.PositiveIntegerField(_("width"), null=True, blank=True)
-    
-    height = models.PositiveIntegerField(_("height"), null=True, blank=True)
-    
+    alt_text = models.CharField(
+        _("alt text"), null=False, blank=False, max_length=255
+    )
+
+    width = models.PositiveIntegerField(_('width'), null=True, blank=True)
+
+    height = models.PositiveIntegerField(_('height'), null=True, blank=True)
+
     def __str__(self):
         return self.alt_text
-    
+
     class Meta:
-        verbose_name = _("SVG image")
-        verbose_name_plural = _("SVG images")
+        verbose_name = _('SVG image')
+        verbose_name_plural = _('SVG images')
